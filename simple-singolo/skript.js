1b76e8acab657c12136ef963a7ced3b8fe29c0ff
AleksandraChehovich <sanunia22@gmail.com>
2020-10-19 19:55:01 +0300

    feat: fix anchors on js

--- simple-singolo/skript.js
+++ simple-singolo/skript.js
@@ -34,9 +34,8 @@ function moveRight () {
 
 let totalFilter = document.getElementsByClassName("filter_item"),
     totalNavItems = document.getElementsByClassName("navigation_item"),
-    portfolioImages = document.querySelector("portfolio_images");
-
-   
+    portfolioImages = document.querySelector("portfolio_images"),
+    portfolioArray = document.getElementsByClassName("images_item");
 
     function showActiveFilter (event) {
         event.preventDefault();
@@ -67,3 +66,50 @@ for (let i = 0; i <  totalNavItems.length; i++) {
 
     totalNavItems[i].addEventListener('click', showActiveNav, false);
 }
+
+let burgerHide = document.querySelector(".burger-page_wrapper"),
+    burgerOpen = document.querySelector(".burger_label"),
+    burgerClose = document.querySelector(".burger-page_close");
+
+function closeBurgerMenu (event) {
+    event.preventDefault();
+    document.querySelector(".burger-page").style.left = "-768px";
+
+}
+
+function openBurgerMenu (event) {
+    event.preventDefault();
+    document.querySelector(".burger-page").style.left = "0"
+}
+
+console.log(burgerOpen);
+
+ burgerHide.addEventListener('click', closeBurgerMenu);
+ 
+ burgerOpen.addEventListener('click', openBurgerMenu);
+
+burgerClose.addEventListener('click', closeBurgerMenu);
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+
+    } return array;
+  }
+
+  const anchors = document.querySelectorAll('a[href*="#"]')
+
+  for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault()
+      
+      const blockID = anchor.getAttribute('href').substr(1)
+      
+      document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      })
+    })
+  }
+ 
