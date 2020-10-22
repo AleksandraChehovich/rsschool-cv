78d738f02fd23b45919eeee85f3bcb3745406559
AleksandraChehovich <sanunia22@gmail.com>
2020-10-22 11:34:54 +0300

    feat: create a calculator

---
+++ calculator/script.js
@@ -0,0 +1,33 @@
+const clearButton = document.querySelectorAll(".calc_button__clear");
+const numberButton = document.querySelectorAll(".calc_button__number");
+const operationButton = document.querySelectorAll(".calc_button__operation");
+const pointButton = document.querySelector(".calc_button__point");
+const resultButton = document.querySelector(".calc_button__result");
+const display = document.querySelector(".calc_display__input");
+
+console.log(display);
+
+
+const getCalculatedNumber = () => {
+
+}
+
+const enterDataNumber = () => {
+
+}
+
+const enterOperation = () => {
+
+}
+
+const getResult = () => {
+
+}
+
+const clearDisplay = () => {
+
+}
+
+const putDecimalPoint = () => {
+
+}
