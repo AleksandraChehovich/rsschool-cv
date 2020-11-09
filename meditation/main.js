24203afbb3437e0dafe7155404833d16f57f6d5c
AleksandraChehovich <sanunia22@gmail.com>
2020-11-09 10:16:13 +0300

    feat: fixed and improved functions in js

--- meditation/main.js
+++ meditation/main.js
@@ -6,14 +6,13 @@ const button = document.querySelector('.button_svg');
 const video = document.querySelector('video');
 const track = document.querySelector('.track');
 const timerSmall = document.querySelector('.time-input');
-let currentDuration = 300;
+const navigation = document.querySelector('.nav_menu');
 const timerBtn = document.querySelectorAll('.timer');
-
+let currentDuration = 300;
 
 const play = (btn) => {
     if (btn.classList.contains('active')) {
-        video.pause();
-        track.pause();
+        pauseMedia();
         btn.classList.remove('active');
         btn.children[0].src = './svg/play.svg';
     } else {
@@ -21,59 +20,72 @@ const play = (btn) => {
             buttons[i].classList.remove('active');
             buttons[i].children[0].src = './svg/play.svg';
         }
-        video.play();
-        track.play();
+        playMedia()
         btn.classList.add('active');
         btn.children[0].src = './svg/stop.svg';
     }
 }
 
-
-for (let i = 0; i < buttons.length; i++) {  
-    buttons[i].addEventListener('click', function (e) {         
-        track.src = this.getAttribute('data-sound');
-        video.src = this.getAttribute('data-video');
-        play(e.currentTarget);
-    });
+const playMedia = () => {
+    video.play();
+    track.play();
 }
+
+const pauseMedia = () => {
+    video.pause();
+    track.pause();
+}
+
+const openMenu = () => {
+    navBar.style.left = '0';
+    svgToClose.style.display = 'block';
+    svgToOpen.style.display = 'none';
+}
+
+const closeMenu = () => {
+    navBar.style.left = '-200px';
+    svgToClose.style.display = 'none';
+    svgToOpen.style.display = 'block';
+    svgToOpen.style.top = '45%';
+}
+
+const addFirstZero = (num) => num.toString().padStart(2, '0');
+
 
 track.ontimeupdate = function() {
     let currentTime = track.currentTime;
     let residual = currentDuration - currentTime;
     let min = Math.floor(residual / 60);
     let sec = Math.floor(residual % 60);
-    if (sec < 10) {
-        sec = `0${sec}`;
-    }
-    timerSmall.textContent = `${min}:${sec}`;
+    timerSmall.textContent = `${addFirstZero(min)}:${addFirstZero(sec)}`;
     if (currentTime > currentDuration) {
-        track.pause();
-        video.pause();
+        pauseMedia();
         track.currentTime = 0;
     }
 }
 
-for (let i = 0; i < timerBtn.length; i++) {
-    timerBtn[i].addEventListener('click', (e) => {
-       let currentBtn = e.target;
-        currentDuration = e.currentTarget.getAttribute('data-time');
-        if (currentDuration === 'Infinity') {
-            timerSmall.textContent = 'infinity';
-        } else {
-            timerSmall.textContent = `${Math.floor(currentDuration / 60)}:${Math.floor(currentDuration % 60)}`;
-        }
-    })
+const setTimer = () => {
+    let startTime = timerBtn.getAttribute('data-time');
+    let  minStart = Math.floor(startTime / 60);
+    let secStart = Math.floor(startTime % 60);
+        
 }
 
-svgToClose.addEventListener('click', () => {
-    navBar.style.left = '-200px';
-    svgToClose.style.display = 'none';
-    svgToOpen.style.display = 'block';
-    svgToOpen.style.top = '45%';
-});
+navigation.onclick = function (event) {
+    let currentButton = event.target;
+    if (currentButton.classList.contains('timer')) {
+        currentDuration = currentButton.getAttribute('data-time');
+        timerSmall.textContent = `${addFirstZero(Math.floor(currentDuration / 60))}:${addFirstZero(Math.floor(currentDuration % 60))}`;  
+    } else if (currentButton.classList.contains('navigation')) {
+        if (currentButton.tagName === 'BUTTON') {
+            currentButton = currentButton;
+        } else currentButton = currentButton.closest('button');
+        track.src = currentButton.getAttribute('data-sound');
+        video.src = currentButton.getAttribute('data-video');
+        play(currentButton);
+    } else return  null;
+}
 
-svgToOpen.addEventListener('click', ()=> {
-    navBar.style.left = '0';
-    svgToClose.style.display = 'block';
-    svgToOpen.style.display = 'none';
-});
+svgToClose.addEventListener('click', closeMenu);
+
+svgToOpen.addEventListener('click', openMenu);
